2d70cb166fc83bb98750a9e81d9243df850b7a8d
gabrielelibardi <gabrielelibardi@yahoo.it>
2021-02-08 18:33:31 +0100

    self made convolutions work

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import multiprocessing as mp
 
 
 
@@ -18,3 +19,75 @@ class CAModel(nn.Module):
         for _ in range(steps):
             x = self.transition(torch.softmax(x, dim=1))
         return x
+    
+    
+    
+class CAModelRecurrent(nn.Module):
+    def __init__(self, num_states):
+        super(CAModel, self).__init__()
+        self.transition = nn.Sequential(
+            nn.Conv2d(num_states, 128, kernel_size=5, padding=2),     
+            nn.ReLU(),
+            nn.Dropout(0.05),
+            nn.Conv2d(128, num_states, kernel_size=1)
+        )
+        
+    def forward(self, x, steps=1):
+        for _ in range(steps):
+            x = self.transition(torch.softmax(x, dim=1))
+        return x
+
+class LSTMCell:
+
+    def __init__(self, input_size, hidden_size):
+        self.input_size=input_size
+        self.W_full = torch.FloatTensor(input_size + hidden_size, 
+        hidden_size*4).type(dtype).to(device)
+        init.normal(self.W_full, 0.0, 0.4)
+        self.W_full = Variable(self.W_full, requires_grad = True)
+        self.bias=1.0
+        self.forget_bias=1.0
+
+    def __call__(self, x, h, c):
+
+        concat = torch.cat((x, h), dim=1)
+        hidden = torch.matmul(concat, self.W_full)+self.bias
+
+        i, g, f, o = torch.chunk(hidden, 4, dim=1)
+
+        i = torch.sigmoid(i)
+        g = torch.tanh(g)
+        f = torch.sigmoid(f+self.forget_bias)
+        o = torch.sigmoid(o)
+
+        new_c = torch.mul(c, f) + torch.mul(g, i)
+        new_h = torch.mul(torch.tanh(new_c), o)
+
+        return new_h, new_c
+    
+    
+class CAModelMeta(nn.Module):
+    def __init__(self, num_states, n_channels=11):
+        super(CAModelMeta, self).__init__()
+        self.n_channels = n_channels
+        self.kernel_size = 3
+        # First step rewrite transition using torch.nn.Unfold
+        self.transition = nn.Sequential(
+            nn.Linear(self.n_channels*self.kernel_size**2, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.n_channels)
+        )
+        
+    def forward(self, x, steps=1):
+        for _ in range(steps):
+            x = self.convolve(torch.softmax(x, dim=1))
+        return x
+    
+    def convolve(self, x, steps=1):
+
+        windows = F.unfold(x, kernel_size=3, stride = 1, padding = 1)
+        output = self.transition(windows.permute([2,0,1]))
+        return output.permute(1,2,0).view(x.shape)
+        
+        
+    
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 from data_loader import DatasetARC
-from models import CAModel
+from models import CAModel, CAModelMeta
 import torch.nn as nn
 import torch.nn.functional as F
 from utils import inp2img
@@ -9,7 +9,7 @@ from utils import inp2img
 
 def solve_tasks(tasks):
     for task in tasks:
-        solve_task(task)
+        solve_task_meta(task)
 
 
 def solve_task(task, max_steps=10):
@@ -48,6 +48,45 @@ def solve_task(task, max_steps=10):
             losses[(num_steps - 1) * num_epochs + e] = loss.item()
             
     return model, num_steps, losses
+
+
+def solve_task_meta(task, max_steps=10):
+    model = CAModelMeta(11).to(device)
+    model = model.train()
+    num_epochs = 100
+    criterion = nn.CrossEntropyLoss()
+    losses = np.zeros((max_steps - 1) * num_epochs)
+
+    for num_steps in range(1, max_steps):
+        optimizer = torch.optim.Adam(model.parameters(), lr=(0.1 / (num_steps * 2)))
+        
+        for e in range(num_epochs):
+            optimizer.zero_grad()
+            loss = 0.0
+
+            for sample in task['train']:
+                # predict output from input
+                x = torch.from_numpy(inp2img(sample["input"])).unsqueeze(0).float().to(device)
+                y = torch.from_numpy(sample["output"]).unsqueeze(0).long().to(device)
+                
+                y_pred = model(x, num_steps)
+                
+                loss += criterion(y_pred, y)
+                
+                # predit output from output
+                # enforces stability after solution is reached
+                y_in = torch.from_numpy(inp2img(sample["output"])).unsqueeze(0).float().to(device)
+                y_pred = model(y_in, 1) 
+                loss += criterion(y_pred, y)
+                
+
+            loss.backward()
+            optimizer.step()
+            
+            losses[(num_steps - 1) * num_epochs + e] = loss.item()
+    print('------------------FINAL LOSS:', loss, '-------------------------------')      
+    return model, num_steps, losses
+
 
 
 if __name__ == '__main__':
